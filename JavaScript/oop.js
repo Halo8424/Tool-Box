2b8cb498206942ef214b88e65073b31217b9a189
Richard.C <r.conde@live.com>
2022-01-01 09:25:49 -0500

    4 pillars of OOP

--- JavaScript/oop.js
+++ JavaScript/oop.js
@@ -1,3 +1,5 @@
+// 4 Pillars of OOP
+
 // Encapsulation: 
     // Is the packing of data and functions into one Component and then controling access to that 
     // component to make a "black box" out of the Object. Because of this, a user of that class only 
@@ -24,3 +26,13 @@ let employee = {
         return this.baseSalary + (this.overTime * this.rate);
     }
 }
+
+// Abstraction: A way of hiding the implementation details and showing only the functionality to the user.
+    // Benefits: Simpler Interface and Reduce Impact of change. 
+
+// Inheritance: Is a mechanisum that helps eliminate redundant code. 
+    // Example: html elements like (textbox), (dropDown-list), (CheckBox) have common properties like "hidden, innerHTML"
+    // and properties like click(), and focus(). Instead of re-writing these for every html element. We define them once in a generic object.
+    // and have other objects Inherit these properties and methods. 
+
+// Polymorphism: Allows us to get rid of long "if and else" and "switch & case" statements. 
