c10d00c86a9a36e2aeb44d8c230596602aa3e982
Richard.C <r.conde@live.com>
2022-01-04 13:08:47 -0500

    Iterating Instance and Prototype members

--- JavaScript/Prototypes.js
+++ JavaScript/Prototypes.js
@@ -27,3 +27,16 @@
     Circle.prototype.draw = function(){
         console.log('draw');
     };
+    // You can refrence an instance method or property from a prototype member and vice versa
+
+// Iterating Instance & Prototype Members
+
+    // Creating a new circle object from the Circle constructor
+    const c1 = new Circle(1);
+
+    // Iterating the Instance members of the c1 object.
+    console.log(Object.keys(c1)) // Expected output ['radius']. Object.keys() will only Iterate Instance members.
+
+    // Iterating the c1 object for instance and prototype members
+    for( let key in c1) console.log(key);  // Expected output: radius draw.
+
