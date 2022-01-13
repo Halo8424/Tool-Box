2ce5a267816c742af74107588095fda0398fd269
Richard.C <r.conde@live.com>
2022-01-13 12:44:29 -0500

    circle & square constructors, circle&square receiving prototype inheritance

--- JavaScript/Prototypes.js
+++ JavaScript/Prototypes.js
@@ -13,3 +13,17 @@
     };   
         
 
+// --- Creating your own Prototypical Inheritance
+    // -- Example of a circle & square constructor using Prototypical Inheritance, Using the "drawTriangle method from above"
+
+    function Circle(radius){
+        this.radius = radius;
+    }
+
+    function Square(){};
+
+    // -- Example of using Inheritance
+        Circle.prototype = Object.create(Triangle.prototype);
+        Square.prototype = Object.create(Triangle.prototype);
+
+        
