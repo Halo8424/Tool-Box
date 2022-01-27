b32f35a2f111fb7595b8a2387742c26015662939
Richard.C <r.conde@live.com>
2022-01-27 12:32:56 -0500

    multiple conditional ternary operator

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -8,9 +8,17 @@
 
 // Use the Conditional (Ternary) Operator
 
-    // Checking for strict equality Example
+    // Example of Checking for strict equality 
     function checkForEquality(a,b){
         return a === b ? "'a' and 'b' are Equal " : "Not Equal";
     };
-
     // Note - You can change global variables within these Ternary Operators
+
+
+    // Example of multiple conditional Ternary Operator
+    // a function that checks if a number is zero even or odd
+    function checkNumber(num){
+        return num === 0 ? "zero"
+        : num % 2 === 0 ? "even"
+        : "odd";
+    }
